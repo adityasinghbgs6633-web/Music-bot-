1891d60f820f8ac8fb40dbbad1ff01e968b1e9fc
adityasinghbgs6633-web <adityasinghbgs6633@gmail.com>
2026-07-10 23:39:11 +0530

    Add files via upload

---
+++ Melody/utils/autoclear.py
@@ -0,0 +1,21 @@
+import os
+import gc
+
+from config import autoclean
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                if rem.startswith("http"):
+                    return
+                try:
+                    os.remove(rem)
+                    gc.collect()
+                except:
+                    pass
+    except:
+        pass
